8a1417c2a1fdc0b6757bb62aeafdbeeb4b224a93
Niet794 <0251MCA311@niet.co.in>
2025-11-04 14:00:44 -0800

    Initial commit: Created demo app with 2 buttons

---
+++ app/src/main/java/com/example/gitdemoapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.gitdemoapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnHello = findViewById<Button>(R.id.btnHell)
+        val btnExit = findViewById<Button>(R.id.btnExi)
+
+        btnHello.setOnClickListener {
+            Toast.makeText(this, "Hello Ayyub!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnExit.setOnClickListener {
+            finish()
+            exitProcess(0)
+        }
+    }
+}
